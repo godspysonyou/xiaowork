8655d16fc12c3f0dce0ace3533084dc28a0345c4
hduwqw <17816124660@163.com>
2018-06-11 20:12:58 +0800

    6-8

--- maindo/xio_figureplot.py
+++ maindo/xio_figureplot.py
@@ -3,7 +3,7 @@ import uifiles.xio_figureplot_ui as ui
 import sys
 import threading
 from utils.utils import Timer
-from figure.figure_plot import Figure_Pie
+from figure.figure_plot import Figure_Pie,Figure_MT,Figure_Loss,Figure_OEE,Figure_Origin
 
 
 class XioFigurePlot(QtGui.QWidget):
@@ -28,15 +28,34 @@ class XioFigurePlot(QtGui.QWidget):
             self.ui.graphicsView_Pie.show()
 
         def draw_oee(): # 绘制oee日推图
-            pass
+            oee=Figure_OEE()
+            oee.plot()#参数
+            graphicscene_oee=QtGui.QGraphicsScene()
+            graphicscene_oee.addWidget(oee.canvas)
+            self.ui.graphicsView_OEE.setScene(graphicscene_oee)
+            self.ui.graphicsView_OEE.show()
+
 
         def draw_loss(): # 绘制损失直方图
-            pass
+            loss=Figure_Loss()
+            loss.plot()
+            graphicscene_loss=QtGui.QGraphicsScene()
+            graphicscene_loss.addWidget(loss.canvas)
+            self.ui.graphicsView_Loss.setScene(graphicscene_loss)
+            self.ui.graphicsView_Loss.show()
 
         def draw_mt(): # 绘制耗材使用图
-            pass
-
+            mt=Figure_MT()
+            mt.plot()
+            graphicscene_mt=QtGui.QGraphicsScene()
+            graphicscene_mt.addWidget(mt.canvas)
+            self.ui.graphicsView_MT.setScene(graphicscene_mt)
+            self.ui.graphicsView_MT.show()
         draw_fp()
+        draw_loss()
+        draw_mt()
+        draw_oee()
+
 
 
 if __name__ == '__main__':
