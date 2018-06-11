6c7497f13fcf0d462b9d32b25cea4e8ee2ffb041
godspysonyou <1344094556@qq.com>
2018-06-11 14:46:21 +0800

    2018/6/8

--- utils/utils.py
+++ utils/utils.py
@@ -36,7 +36,7 @@ def show_image(image):
 
 def roi_cut(filepath='../images/50.jpg', loc=(5, 130, 980, 1090)):
     '''
-    cut roi in a image
+    截取一个矩形兴趣区域
     :param filepath:
     :param loc:
     :return:
@@ -45,19 +45,34 @@ def roi_cut(filepath='../images/50.jpg', loc=(5, 130, 980, 1090)):
     l = image[loc[0]:loc[1], loc[2]:loc[3]]
     cv2.imwrite(filepath[0:-4] + '1.jpg', l)
 
+def gamma_trans(img,gamma):
+    #具体做法先归一化到1，然后gamma作为指数值求出新的像素值再还原
+    gamma_table = [np.power(x/255.0,gamma)*255.0 for x in range(256)]
+    gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
+    #实现映射用的是Opencv的查表函数
+    return cv2.LUT(img,gamma_table)
+
 
 if __name__ == '__main__':
     # show_image('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/447.jpg')
-    img = cv2.imread('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/447.jpg')
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.imread('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/280.jpg')
+    img_gam = gamma_trans(img,0.45)
 
-    lower = np.array([-60, 20, 80])
-    upper = np.array([80, 80, 250])
+    hsv = cv2.cvtColor(img_gam, cv2.COLOR_BGR2HSV)
+
+    lower = np.array([55, 100, 200])
+    upper = np.array([140, 180, 255])
     mask = cv2.inRange(img, lower, upper)
 
-    res = cv2.bitwise_and(img, img, mask=mask)
+    res = cv2.bitwise_and(img_gam, img_gam, mask=mask)
 
-    #print(img[250,700])
+    print(img[240, 660])
+    print(img[420,750])
 
+
+    show_image('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/447.jpg')
+    # cv2.imshow('img',img)
+    cv2.imshow('ga', img_gam)
+    cv2.imshow('mask', mask)
     cv2.imshow('res', res)
     cv2.waitKey(0)
