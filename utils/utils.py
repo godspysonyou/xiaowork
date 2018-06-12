a78455375fa52531e78d6c68e510f89a30340c1a
godspysonyou <1344094556@qq.com>
2018-06-12 10:59:51 +0800

    2018/6/11

--- utils/utils.py
+++ utils/utils.py
@@ -45,34 +45,106 @@ def roi_cut(filepath='../images/50.jpg', loc=(5, 130, 980, 1090)):
     l = image[loc[0]:loc[1], loc[2]:loc[3]]
     cv2.imwrite(filepath[0:-4] + '1.jpg', l)
 
-def gamma_trans(img,gamma):
-    #具体做法先归一化到1，然后gamma作为指数值求出新的像素值再还原
-    gamma_table = [np.power(x/255.0,gamma)*255.0 for x in range(256)]
+
+def gamma_trans(img, gamma):
+    '''
+    将图像光照归一化
+    :param img:
+    :param gamma:
+    :return:
+    '''
+    # 具体做法先归一化到1，然后gamma作为指数值求出新的像素值再还原
+    gamma_table = [np.power(x / 255.0, gamma) * 255.0 for x in range(256)]
     gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
-    #实现映射用的是Opencv的查表函数
-    return cv2.LUT(img,gamma_table)
+    # 实现映射用的是Opencv的查表函数
+    return cv2.LUT(img, gamma_table)
 
 
-if __name__ == '__main__':
+def find_spark(img, spark_roi):
+    '''
+    在固定区域检测火花
+    :param img:
+    :param spark_roi: 这个参数为一个不规则mask，二值图
+    :return:
+    '''
+    img_gam = gamma_trans(img, 0.75)  # 光照归一化
+    hsv = cv2.cvtColor(img_gam, cv2.COLOR_BGR2HSV)
+
+    # 火花颜色
+    lower = np.array([0, 0, 250])
+    upper = np.array([360, 10, 255])
+    mask = cv2.inRange(hsv, lower, upper)
+    #res_1 = cv2.bitwise_and(hsv,hsv,mask=mask) # hsv颜色选取火花颜色
+    res = cv2.bitwise_and(mask,mask,mask=spark_roi) # 在该区域检索
+    res /= 255
+    s = np.sum(res)
+    if s>6000:
+        return True
+    return False
+
+
+def find_spark_test():
+    cap = cv2.VideoCapture('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/videos/left_cam.mp4')
+    while (1):
+        # Take each frame
+        _, img = cap.read()
+
+        # Convert BGR to HSV
+
+        img_gam = gamma_trans(img, 0.75)
+        img0 = cv2.imread('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/mask.jpg', 0)
+        ret, thresh1 = cv2.threshold(img0, 127, 255, cv2.THRESH_BINARY)
+        hsv = cv2.cvtColor(img_gam, cv2.COLOR_BGR2HSV)
+
+        lower = np.array([0, 0, 250])
+        upper = np.array([360, 10, 255])
+        mask = cv2.inRange(hsv, lower, upper)
+        res = cv2.bitwise_and(img_gam, img_gam, mask=mask)
+        res_1 = cv2.bitwise_and(res, res, mask=thresh1)
+        num_res_1 = res_1 / 255
+        print(np.sum(num_res_1))
+        cv2.imshow('ga', img_gam)
+        cv2.imshow('mask', mask)
+        cv2.imshow('res', res)
+        cv2.imshow('res1', res_1)
+        cv2.waitKey(1)
+
+
+def test1():
     # show_image('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/447.jpg')
-    img = cv2.imread('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/280.jpg')
-    img_gam = gamma_trans(img,0.45)
+    img = cv2.imread('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/jietu.jpg')
+
+    #
+    img0 = cv2.imread('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/gam_s.jpg', 0)
+    ret, thresh1 = cv2.threshold(img0, 127, 255, cv2.THRESH_BINARY)
+    # cv2.imshow('2', thresh1)
+    # cv2.waitKey(0)
+
+    img_gam = gamma_trans(img, 0.75)  # 亮度归一化
+    # cv2.imwrite('gam.jpg',img_gam)
 
     hsv = cv2.cvtColor(img_gam, cv2.COLOR_BGR2HSV)
 
-    lower = np.array([55, 100, 200])
-    upper = np.array([140, 180, 255])
-    mask = cv2.inRange(img, lower, upper)
+    lower = np.array([0, 75, 200])
+    upper = np.array([20, 150, 255])
+    mask = cv2.inRange(hsv, lower, upper)
 
     res = cv2.bitwise_and(img_gam, img_gam, mask=mask)
 
-    print(img[240, 660])
-    print(img[420,750])
+    print(img_gam[190, 740])
+    print(img_gam[200, 700])
 
-
-    show_image('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/447.jpg')
+    # show_image('/Users/kaimingcheng/PycharmProjects/xiaowork/maindo/images/jietu.jpg')
     # cv2.imshow('img',img)
     cv2.imshow('ga', img_gam)
     cv2.imshow('mask', mask)
     cv2.imshow('res', res)
     cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    # test1()
+    find_spark_test()
+    # s = np.array([[1,0],
+    #          [1,1]])
+    # print(np.sum(s))
