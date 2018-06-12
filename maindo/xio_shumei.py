1d6afde34ec40e3552edb5b0c92f767f15c3b967
hduwqw <17816124660@163.com>
2018-06-12 16:21:58 +0800

    6-12 wang

--- maindo/xio_shumei.py
+++ maindo/xio_shumei.py
@@ -1,7 +1,8 @@
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtNetwork import *
-import uifiles.xio_shumei_ui as ui
+import uifiles.xio_shumei as ui
 import sys
+import time
 
 import socket
 
@@ -22,11 +23,36 @@ class XioShuMei(QtGui.QWidget):
         super(XioShuMei, self).__init__()
         self.ui = ui.Ui_Form()
         self.ui.setupUi(self)
+        self.type="end"
 
 
     def keyPressEvent(self, QKeyEvent):
         if QKeyEvent.key() == QtCore.Qt.Key_1: # 这里要设计逻辑避免工人误按
-            client_connect(self.HOST, self.PORT, '1')
+            infoBox = QtGui.QMessageBox()
+            reply = infoBox.question(self, 'Message', '确定操作？', infoBox.Yes | infoBox.No, infoBox.No)
+            if reply == infoBox.Yes:
+                if self.type == "end":
+                    self.ui.thing_label.clear()
+                    self.ui.start_label.clear()
+                    self.ui.end_label.clear()
+                    self.ui.time_label.clear()
+                    self.start_time = time.strftime('%H:%M:%S', time.localtime(time.time()))
+                    self.ui.thing_label.append("工人打孔")
+                    self.ui.thing_label.show()
+                    self.ui.start_label.append(self.start_time)
+                    self.ui.start_label.show()
+                    infoBox = QtGui.QMessageBox()
+                    infoBox.setIcon(QtGui.QMessageBox.Information)
+                    infoBox.setText("打孔操作\n" + self.start_time)
+                    infoBox.setWindowTitle("Information")
+                    infoBox.setStandardButtons(QtGui.QMessageBox.Ok)
+                    infoBox.button(QtGui.QMessageBox.Ok).animateClick(3 * 1000)
+                    infoBox.exec_()
+                    self.ui.caution_browser.append(" 请结束操作！")
+                    self.type = "dakong"
+                    #client_connect(self.HOST, self.PORT, '1')
+            else:
+                pass
 
         elif QKeyEvent.key() == QtCore.Qt.Key_2:
             client_connect(self.HOST, self.PORT, '2')
