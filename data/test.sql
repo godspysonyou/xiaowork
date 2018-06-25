960c5e96fed5ce175c5868063302e5ac3d146038
hduwqw <17816124660@163.com>
2018-06-25 16:45:06 +0800

    6-25 wang

--- data/test.sql
+++ data/test.sql
@@ -1,26 +1,25 @@
 /*
- Navicat MySQL Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : test
- Source Server Type    : MySQL
- Source Server Version : 50714
- Source Host           : localhost
- Source Database       : test
+Source Server         : wqw
+Source Server Version : 50525
+Source Host           : localhost:3306
+Source Database       : test
 
- Target Server Type    : MySQL
- Target Server Version : 50714
- File Encoding         : utf-8
+Target Server Type    : MYSQL
+Target Server Version : 50525
+File Encoding         : 65001
 
- Date: 06/24/2018 15:32:24 PM
+Date: 2018-06-25 16:43:45
 */
 
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
---  Table structure for `DZ`
+-- Table structure for dz
 -- ----------------------------
-DROP TABLE IF EXISTS `DZ`;
-CREATE TABLE `DZ` (
+DROP TABLE IF EXISTS `dz`;
+CREATE TABLE `dz` (
   `SJC` datetime NOT NULL COMMENT '时间戳',
   `GRGH` varchar(20) COLLATE utf8_bin DEFAULT NULL,
   `LJGH` varchar(20) COLLATE utf8_bin DEFAULT NULL,
@@ -29,17 +28,23 @@ CREATE TABLE `DZ` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
---  Records of `DZ`
+-- Records of dz
 -- ----------------------------
-BEGIN;
-INSERT INTO `DZ` VALUES ('2018-06-21 15:37:25', '1A', '0A', 'action1', 'start'), ('2018-06-21 15:38:23', '1A', '0A', 'action1', 'start'), ('2018-06-21 15:41:04', '1A', '0A', 'action1', 'start'), ('2018-06-21 15:41:14', '1A', '0A', 'action2', 'start'), ('2018-06-21 15:41:24', '1A', '0A', 'ction2', 'end'), ('2018-06-21 15:47:17', '1A', '0A', 'action3', 'start'), ('2018-06-21 15:47:33', '1A', '0A', 'ction3', 'end');
-COMMIT;
+INSERT INTO `dz` VALUES ('2018-06-21 15:37:25', '1A', '0A', 'action1', 'start');
+INSERT INTO `dz` VALUES ('2018-06-21 15:38:23', '1A', '0A', 'action1', 'start');
+INSERT INTO `dz` VALUES ('2018-06-21 15:41:04', '1A', '0A', 'action1', 'start');
+INSERT INTO `dz` VALUES ('2018-06-21 15:41:14', '1A', '0A', 'action2', 'start');
+INSERT INTO `dz` VALUES ('2018-06-21 15:41:24', '1A', '0A', 'ction2', 'end');
+INSERT INTO `dz` VALUES ('2018-06-21 15:47:17', '1A', '0A', 'action3', 'start');
+INSERT INTO `dz` VALUES ('2018-06-21 15:47:33', '1A', '0A', 'ction3', 'end');
+INSERT INTO `dz` VALUES ('2018-06-25 16:30:59', '1A', '0A', 'action1', 'start');
+INSERT INTO `dz` VALUES ('2018-06-25 16:31:15', '1A', '0A', 'action1', 'end');
 
 -- ----------------------------
---  Table structure for `LOSS`
+-- Table structure for loss
 -- ----------------------------
-DROP TABLE IF EXISTS `LOSS`;
-CREATE TABLE `LOSS` (
+DROP TABLE IF EXISTS `loss`;
+CREATE TABLE `loss` (
   `SJ` date NOT NULL,
   `ACTION1` int(11) DEFAULT NULL,
   `ACTION2` int(11) DEFAULT NULL,
@@ -48,10 +53,15 @@ CREATE TABLE `LOSS` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
---  Table structure for `OEE_DATE`
+-- Records of loss
 -- ----------------------------
-DROP TABLE IF EXISTS `OEE_DATE`;
-CREATE TABLE `OEE_DATE` (
+INSERT INTO `loss` VALUES ('2018-06-24', '100', '200', '300', '400');
+
+-- ----------------------------
+-- Table structure for oee_date
+-- ----------------------------
+DROP TABLE IF EXISTS `oee_date`;
+CREATE TABLE `oee_date` (
   `SJC` date NOT NULL,
   `O8` float DEFAULT NULL,
   `O9` float DEFAULT NULL,
@@ -67,27 +77,27 @@ CREATE TABLE `OEE_DATE` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
---  Records of `OEE_DATE`
+-- Records of oee_date
 -- ----------------------------
-BEGIN;
-INSERT INTO `OEE_DATE` VALUES ('2018-06-24', '9', null, null, null, null, null, null, null, null, null, null);
-COMMIT;
+INSERT INTO `oee_date` VALUES ('2018-06-24', '80', '82', '85', '78', '60', null, null, null, null, null, null);
 
 -- ----------------------------
---  Table structure for `SC`
+-- Table structure for sc
 -- ----------------------------
-DROP TABLE IF EXISTS `SC`;
-CREATE TABLE `SC` (
+DROP TABLE IF EXISTS `sc`;
+CREATE TABLE `sc` (
   `Sno` mediumtext COLLATE utf8_bin,
   `Cno` int(11) DEFAULT NULL,
   `Grade` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
---  Records of `SC`
+-- Records of sc
 -- ----------------------------
-BEGIN;
-INSERT INTO `SC` VALUES ('200215121', '1', '92'), ('200215121', '2', '85'), ('200215121', '3', '88'), ('200215122', '2', '90'), ('200215122', '3', '80'), ('200215123', '1', '60'), ('200215124', '1', '79');
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `sc` VALUES (0x323030323135313231, '1', '92');
+INSERT INTO `sc` VALUES (0x323030323135313231, '2', '85');
+INSERT INTO `sc` VALUES (0x323030323135313231, '3', '88');
+INSERT INTO `sc` VALUES (0x323030323135313232, '2', '90');
+INSERT INTO `sc` VALUES (0x323030323135313232, '3', '80');
+INSERT INTO `sc` VALUES (0x323030323135313233, '1', '60');
+INSERT INTO `sc` VALUES (0x323030323135313234, '1', '79');
